9c4d11f246557e205bb5e92b40d1778f70aba3a1
lakshmipathi <lakshmipathiroyal2000@gmail.com>
2023-08-30 00:40:49 +0530

    first serios commit

---
+++ src/test/java/com/cognizant/Project_salar/AppTest.java
@@ -0,0 +1,20 @@
+package com.cognizant.Project_salar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
